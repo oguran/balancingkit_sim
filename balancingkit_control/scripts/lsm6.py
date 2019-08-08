5180dc7cfc08854aba337e0bdba1cecd9d2ccb7d
Takuya Ogura <oguran29@gmail.com>
2019-08-08 23:38:31 +0900

    Modify

--- balancingkit_control/scripts/lsm6.py
+++ balancingkit_control/scripts/lsm6.py
@@ -4,45 +4,21 @@ import struct
 import collections
 from sensor_msgs.msg import Imu
 
-#class Regs(object):
-#  CTRL1_XL    = 0x10
-#  CTRL2_G     = 0x11
-#  CTRL3_C     = 0x12
-#  OUTX_L_G    = 0x22
-#  OUTX_L_XL   = 0x28
-
 Vector = collections.namedtuple('Vector', 'x y z')
 
 class LSM6(object):
 
   def __init__(self, slave_addr = 0b1101011):
-    self.bus = smbus.SMBus(1)
-    self.sa = slave_addr
     self.g = Vector(0, 0, 0)
     self.a = Vector(0, 0, 0)
-      
+
   def enable(self):
-#    self.bus.write_byte_data(self.sa, Regs.CTRL1_XL, 0x50) # 208 Hz ODR, 2 g FS
-#    self.bus.write_byte_data(self.sa, Regs.CTRL2_G, 0x58) # 208 Hz ODR, 1000 dps FS
-#    self.bus.write_byte_data(self.sa, Regs.CTRL3_C, 0x04) # IF_INC = 1 (automatically increment register address)
 
   def read(self):
-    self.read_gyro()
-    self.read_accel() 
-
-  def read_gyro(self):
-    byte_list = self.bus.read_i2c_block_data(self.sa, Regs.OUTX_L_G, 6)
-    self.g = Vector(*struct.unpack('hhh', bytes(byte_list)))
-
-  def read_accel(self):
-    byte_list = self.bus.read_i2c_block_data(self.sa, Regs.OUTX_L_XL, 6)
-    self.a = Vector(*struct.unpack('hhh', bytes(byte_list)))
 
   def imu_callback():
-    print "veloc angular z = " + str(msg.angular_velocity.z)
-        print "veloc angular y = " + str(msg.angular_velocity.y)
-            print "aceleracion linear x = " + str(msg.linear_acceleration.x)
-                print "aceleracion linear y = " + str(msg.linear_acceleration.y)
+    self.g = Vector(msg.angular_velocity)
+    self.a = Vector(msg.linear_acceleration)
 
   def listener():
     rospy.init_node("virtual_lsm6", anonymous=True)
